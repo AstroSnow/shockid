a6b431afd9878b720c1f0a9bcb15c514e65db86b
Astrosnow <b.snow@exeter.ac.uk>
2023-02-14 14:37:06 +0000

    Updates of testing routines

--- shockid_py/test2D.py
+++ shockid_py/test2D.py
@@ -13,7 +13,7 @@ from shockid import shockid
 fname='../MHD_2D_rec/'
 fname='../../shockstab/Data/'
 
-ds=pipread(fname,0)
+ds=pipread(fname,10)
     
 v1=ds['vx_p']
 v2=ds['vy_p']
@@ -33,6 +33,8 @@ divv=(v2[margin+1:egy-margin+1,margin:egx-margin]-\
 #plt.contourf(divv[:,950:1150],levels=np.linspace(-0.055,-0.00001,100))
 plt.contourf(divv,levels=101,vmax=-0.01)
 
-[col,row]=shockid(ds['xgrid'],ds['ygrid'],0.0,ds['ro_p'],ds['vx_p'],ds['vy_p'],0.0,ds['bx'],ds['by'],ds['bz'],ds['pr_p'])
+#[col,row]=shockid(ds['xgrid'],ds['ygrid'],0.0,ds['ro_p'],ds['vx_p'],ds['vy_p'],0.0,ds['bx'],ds['by'],ds['bz'],ds['pr_p'])
+#plt.plot(row,col,color='r',linestyle='',marker='.',markersize=2.8)
 
-plt.plot(row,col,color='r',linestyle='',marker='.',markersize=0.8)
+shocks=shockid(ds['xgrid'],ds['ygrid'],0.0,ds['ro_p'],ds['vx_p'],ds['vy_p'],0.0,ds['bx'],ds['by'],ds['bz'],ds['pr_p'])
+plt.plot(shocks['slow'][:,1],shocks['slow'][:,0],color='r',linestyle='',marker='.',markersize=2.8)
